0a724aea321babbc3d0819bdcefc01f1766cfb7d
yuanxin <975341137@qq.com>
2018-05-22 11:15:19 +0800

    xiugai

---
+++ mylibrary/src/main/java/com/chineseall/mylibrary/TestActivity.kt
@@ -0,0 +1,12 @@
+package com.chineseall.mylibrary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
